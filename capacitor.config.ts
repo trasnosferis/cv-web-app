2b7abb84468e2687770172cff82ddb2d1bddf722
trasnosferis <trasnosferis@gmail.com>
2022-06-25 10:59:06 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cv-web-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
